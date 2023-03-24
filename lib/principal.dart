b762c2e49ce853e5c15b64cb0d40229ddfef7a12
Walter Pretzantzín <pretz@cloudit4.com>
2023-03-23 19:15:46 -0600

    Agregando y modificando el boton inferior de la pagina principal

--- lib/principal.dart
+++ lib/principal.dart
@@ -1,60 +1,83 @@
 import 'package:flutter/material.dart';
 
-class Principal extends StatelessWidget {
+void main() => runApp(Principal());
+
+class Principal extends StatefulWidget {
+  @override
+  _MyAplicacion createState() => _MyAplicacion();
+}
+
+class _MyAplicacion extends State<Principal> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("HIDROCOMPRAS"),
-        //color de fondo
-        backgroundColor: Color.fromARGB(255, 0, 5, 77),
-        // Establecer la imagen de la pantalla frontal
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {},
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HIDROCOMPRAS',
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("HIDROCOMPRAS"),
+          //color de fondo
+          backgroundColor: Color.fromARGB(255, 0, 5, 77),
+          // Establecer la imagen de la pantalla frontal
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
 
-        // Coloca el icono detrás
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.account_circle),
-            onPressed: () {
-              Navigator.of(context).pushNamed('/six');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.exit_to_app_rounded),
-            onPressed: () {
-              Navigator.of(context).pushNamed('/one');
-            },
-          ),
-        ],
-      ),
-      body: Text("PRODUCTOS"),
-      backgroundColor: Color.fromARGB(255, 214, 214, 214),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Inicio',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.badge_outlined),
-            label: 'Servicios',
-            backgroundColor: Color.fromARGB(255, 0, 5, 77),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list_outlined),
-            label: 'Actividades',
-            backgroundColor: Color.fromARGB(255, 0, 5, 77),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fire_truck_outlined),
-            label: 'Pedidos',
-          ),
-        ],
-        fixedColor: Colors.red,
+          // Coloca el icono detrás
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/six');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.logout_rounded),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/one');
+              },
+            ),
+          ],
+        ),
+        body: Text("PRODUCTOS"),
+        //backgroundColor: Color.fromARGB(255, 214, 214, 214),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Color.fromARGB(255, 0, 5, 77),
+          selectedItemColor: Color.fromARGB(255, 126, 236, 255),
+          unselectedItemColor: Color.fromARGB(255, 224, 224, 224),
+          currentIndex: 0,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: 'Inicio',
+              backgroundColor: Colors.blue,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.build_circle,
+              ),
+              label: 'Servicios',
+              backgroundColor: Colors.lightBlue,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.assignment,
+              ),
+              label: 'Actividades',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.local_shipping,
+              ),
+              label: 'Pedidos',
+            ),
+          ],
+        ),
       ),
     );
   }
