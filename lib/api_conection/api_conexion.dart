c44394fba29035ca383eb932ff7da03c8377f86d
Walter <pretz@cloudit4.com>
2023-04-21 18:03:27 -0600

    Modificando direccion de la conexion de la api

--- lib/api_conection/api_conexion.dart
+++ lib/api_conection/api_conexion.dart
@@ -1,27 +1,24 @@
 class API {
   //direccion donde se encuentra la api conexion ↓↓↓
-  static const hostConnect =
-      "https://electriconenergy.com.gt/hidroapp/api_plomero";
-  static const hostConnectUser =
-      "https://electriconenergy.com.gt/hidroapp/api_plomero/usuario";
+  static const hostConnect = "http://localhost/api_plomero/";
+  static const hostConnectUser = "http://localhost/api_plomero/usuario";
 
   //registro de nuevo usuario
   static const crearUsuario =
-      "https://electriconenergy.com.gt/hidroapp/api_plomero/usuario/crear_usuario.php";
+      "http://localhost/api_plomero/usuario/crear_usuario.php";
 
   //validar si ya existe el correo electrónico
   static const validarCorreoUsuario =
-      "https://electriconenergy.com.gt/hidroapp/api_plomero/usuario/validar_correo.php";
+      "http://localhost/api_plomero/usuario/validar_correo.php";
 
   //api para iniciar sesión por medio de la app
-  static const loginUsuario =
-      "https://electriconenergy.com.gt/hidroapp/api_plomero/usuario/login.php";
+  static const loginUsuario = "http://localhost/api_plomero/usuario/login.php";
 
   //api para validar si existe registro este correo para recuperar
   static const validarCorreoRegistrado =
-      "https://electriconenergy.com.gt/hidroapp/api_plomero/restaurar/validar_correo.php";
+      "http://localhost/api_plomero/restaurar/validar_correo.php";
 
   //api para actualizar contraseña del usuario
   static const updatePaswordUser =
-      "https://electriconenergy.com.gt/hidroapp/api_plomero/restaurar/actualizar_contrasena.php";
+      "http://localhost/api_plomero/restaurar/actualizar_contrasena.php";
 }
